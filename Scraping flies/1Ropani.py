079398e54fa4e3cc204394c457ab7132762977a3
sanoj23 <sanoj.shrestha.13@gmail.com>
2021-07-08 18:05:18 +0545

    1Ropani data scrapped

--- Scraping flies/1Ropani.py
+++ Scraping flies/1Ropani.py
@@ -10,6 +10,7 @@ soup = BeautifulSoup(r.content, 'html5lib')
 data = []
 
 
+
 Total_content = soup.findAll('div', {'class': 'list_desc'})
 
 
@@ -22,34 +23,33 @@ Total_content = soup.findAll('div', {'class': 'list_desc'})
 
 
 for element in Total_content:
-    # Final_Location=Price=Area= None
-    New = element.findAll('b')
-    print(New)
-    sys.exit()
-    Location = New[1].text
-    print(Location, end=" ")
-    Final_Location = Location[0].replace(',', '|')[0]
-    for_seperation = New[0].text
-    Price = for_seperation.split('|')[0]
-    # print(Price,end=" ")
-    Area = for_seperation.split('|')[1]
-    # print(Area,end=" ")
+	# Final_Location=Price=Area= None
+	New = element.findAll('b')
+	print(New)
+	sys.exit()
+	Location = New[1].text
+	print(Location, end=" ")
+	Final_Location = Location[0].replace(',', '|')[0]
+	for_seperation = New[0].text
+	Price = for_seperation.split('|')[0]
+	# print(Price,end=" ")
+	Area = for_seperation.split('|')[1]
+	# print(Area,end=" ")
 
 
 # f.write(Final_Location.replace("|",",") +" ,"+Final_Price + " ,"+Final_Area +"\n")
 
-    row = [Final_Location, Price, Area]
-    data.append(row)
-    print(row)
-    print('............')
+	row = [Final_Location, Price, Area]
+	data.append(row)
+	print(row)
+	print('............')
 
 
 headers = ['Location', 'Price', 'Area']
 with open('csv-flies/1Ropani.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(headers)
-    writer.writerows(data)
-
+	writer = csv.writer(f)
+	writer.writerow(headers)
+	writer.writerows(data)
 
 # f.close()
 
--- Scraping flies/1Ropani_links.py
+++ Scraping flies/1Ropani_links.py
@@ -1,15 +1,10 @@
-from base64 import b16encode
-from os import link
 import requests
 from bs4 import BeautifulSoup
 import csv
 from selenium import webdriver
 import time
 import sys
-
-
-
-
+import os
 
 # function to get links from the 1Ropani website
 def get_links():
@@ -21,6 +16,7 @@ def get_links():
     driver.get(URL)
     select = driver.find_element_by_id('ContentPlaceHolder1_HouseListControl1_ItemsPerPageDropDownList')
     all_options = select.find_elements_by_tag_name("option")
+    #set the results to 100 results per page
     for option in all_options:
         if option.text == '100':
             option.click()
@@ -29,9 +25,8 @@ def get_links():
             # first row and last row contains navigation links and all inbetween contains the house links
             # get all links from the navigation table and store it in a csv file named as 1Ropani_links.csv
 
-
+    # looping throuh 7 pages after setting 100 results per page.
     for i in range(1,8):
-
         pagination = driver.find_element_by_class_name('search_result_pagi')
         indexes = pagination.find_elements_by_tag_name('a')
         for index in indexes:
@@ -45,7 +40,7 @@ def get_links():
         for row in table_rows[2:-2]:
             link = []
             link.append(row.find_element_by_tag_name('a').get_attribute('href'))
-            # check if the link contains apartment
+            # check if the link contains apartment and skip if it is an apartment
             if 'apartment' in link[0].lower():
                 continue
             print("Number: " + str(count) + " : " + link[0])
@@ -54,7 +49,7 @@ def get_links():
 
 
 
-            #save links in a csv file named as 1Ropani_links.csv
+    #save links in a csv file named as 1Ropani_links.csv
     with open('csv-files/1Ropani_links.csv', 'w',newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
@@ -62,15 +57,9 @@ def get_links():
 
 
 
-# remove rows from 1Ropani_links.csv where links contain the string "Apartment"
-def remove_apartment():
-    with open('csv-files/1Ropani_links.csv', 'r') as f:
-        reader = csv.reader(f)
+def ek_ropani():
+    headers = ['title','price','location','district','floor','room','bedroom','bathroom','livingroom','kitchen','parking','link']
 
-
-
-def Ropani():
-    data = []
     # get links from 1Ropani_links.csv
     links = []
     with open('csv-files/1Ropani_links.csv', 'r') as f:
@@ -79,64 +68,81 @@ def Ropani():
         for row in reader:
             links.append(row[0])
 
+    # #check if 1Ropani_complete is empty
+    with open ("csv-files/1Ropani_complete.csv",'r', newline='',encoding="utf-8") as f:
+        reader = csv.reader(f)
+        if list(reader) == []:
+            write_headers()
     #slicing to remove column name
     links = links[1:]
-    # slicing 10 links for testing
 
-    links = links[:30]
+    # store links in csv 1Ropani_complete.csv in stored_links vaiable
+
+    with open('csv-files/1Ropani_complete.csv', 'r') as f:
+        reader = csv.reader(f)
+        stored_links = []
+        for row in reader: stored_links.append(row[-1])
+
     for link in links:
-        source = requests.get(link).text
-        soup = BeautifulSoup(source, 'lxml')
-        soup = soup.find('div',{'id':'property_detail'})
-        title = soup.find('h2').get_text(strip=True)
-        print("Title: ", link)
-        price = soup.find('span',{'class':'price'}).get_text(strip=True).split('|')[0]
-        if "on call" in price:
-            continue
-        else:
-            price = price.split('Rs.')[1].strip()
-        print("Price: ",price)
-        location = soup.find('td',attrs = {'style' : 'width: 200px; padding-left: 5px; padding-right: 15px;'}).get_text(strip=True).split('Land Description:')[0].split(':')[1]
-        print('location: ',location)
-        district = location.split(',')[0]
-        print("district: ",district)
-        area = soup.find('td',attrs = {'style' : 'width: 200px; padding-left: 5px; padding-right: 15px;'}).get_text(strip=True).split('Price:')[0].split('Area:')[1].strip()
-        print('area: ',area)
-        #feature_list = ul with floor, bedroom, bath, kitchen, living room
-        feature_list = soup.find('ul',{'class':'feature_list'})
-        for li in feature_list.find_all('li'):
-            if 'floor' in li.get_text().lower():
-                floor = li.get_text(strip=True)
-            if 'bed' in li.get_text().lower():
-                bedroom = li.get_text(strip=True)
-            if 'bath' in li.get_text().lower():
-                bathroom = li.get_text(strip=True)
-            if 'living' in li.get_text().lower():
-                livingroom = li.get_text(strip=True)
-            if 'kitchen' in li.get_text().lower():
-                kitchen = li.get_text(strip=True)
-        print("floor: ",floor)
-        print("bedroom: ",bedroom)
-        print("livingroom: ",livingroom)
-        print("bathroom: ",bathroom)
+        if link not in stored_links:
+            source = requests.get(link).text
+            soup = BeautifulSoup(source, 'lxml')
+            soup = soup.find('div',{'id':'property_detail'})
+            title = soup.find('h2').get_text(strip=True)
+            print(links.index(link))
+            print("Title: ", title)
+            price = soup.find('span',{'class':'price'}).get_text(strip=True).split('|')[0]
+            if "on call" in price:
+                continue
+            else:
+                price = price.split('Rs.')[1].strip()
+            print("Price: ",price)
+            location = soup.find('td',attrs = {'style' : 'width: 200px; padding-left: 5px; padding-right: 15px;'}).get_text(strip=True).split('Land Description:')[0].split(':')[1]
+            print('location: ',location)
+            district = location.split(',')[0]
+            print("district: ",district)
+            area = soup.find('td',attrs = {'style' : 'width: 200px; padding-left: 5px; padding-right: 15px;'}).get_text(strip=True).split('Price:')[0].split('Area:')[1].strip()
+            print('area: ',area)
+            #feature_list = ul with floor, bedroom, bath, kitchen, living room
+            feature_list = soup.find('ul',{'class':'feature_list'})
+            for li in feature_list.find_all('li'):
+                if 'floor' in li.get_text().lower():
+                    floor = li.get_text(strip=True)
+                if 'bed' in li.get_text().lower():
+                    bedroom = li.get_text(strip=True)
+                if 'bath' in li.get_text().lower():
+                    bathroom = li.get_text(strip=True)
+                if 'living' in li.get_text().lower():
+                    livingroom = li.get_text(strip=True)
+                if 'kitchen' in li.get_text().lower():
+                    kitchen = li.get_text(strip=True)
+            print("floor: ",floor)
+            print("bedroom: ",bedroom)
+            print("livingroom: ",livingroom)
+            print("bathroom: ",bathroom)
 
-        if 'parking' in soup.text.lower():
-            parking = 1
-        else:
-            parking = 0
-        print(parking)
-        room = None # It does not provide total rooms
-        print('----------------------------------------------------------------')
-        row = [title,price,location,district,floor,room,bedroom,bathroom,livingroom,kitchen,parking,link]
-        data.append(row)
+            if 'parking' in soup.text.lower():
+                parking = 1
+            else:
+                parking = 0
+            print(parking)
+            room = None # It does not provide total rooms
+            print('----------------------------------------------------------------')
+            row = [title,price,location,district,floor,room,bedroom,bathroom,livingroom,kitchen,parking,link]
+            with open ("csv-files/1Ropani_complete.csv",'a', newline='',encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerow(row)
 
-        headers = ['title','price','location','district','floor','room','bedroom','bathroom','livingroom','kitchen','parking','link']
-        with open ("csv-files/1Roapni.csv",'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(headers)
-            writer.writerows(data)
+def write_headers():
+    headers = ['title','price','location','district','floor','room','bedroom','bathroom','livingroom','kitchen','parking','link']
+    with open('csv-files/1Ropani_complete.csv', 'w',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
 
-Ropani()
+
+ek_ropani()
 
 # Notes
 # This website has no total rooms and gives details about parking cars or bikes etc.
+
+# which is the best framewrork
